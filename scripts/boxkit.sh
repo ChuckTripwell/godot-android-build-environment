648690d285573d6de269014eaf58084a2104b543
ChuckTripwell <levif58@walla.co.il>
2026-08-20 01:21:45 +0000

    Update boxkit.sh

--- scripts/boxkit.sh
+++ scripts/boxkit.sh
@@ -6,3 +6,5 @@
 # Update the container and install packages
 apk update && apk upgrade
 grep -v '^#' ./boxkit.packages | xargs apk add
+
+echo "cd /home/$USER/.var/app/io.github.MakovWait.Godots/data/godot/app_userdata/Godots/versions/" >> /etc/profile
