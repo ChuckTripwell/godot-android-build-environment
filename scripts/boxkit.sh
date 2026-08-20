977b106fcee48e3777af5a32656d1c377a64bb9e
ChuckTripwell <levif58@walla.co.il>
2026-08-20 03:23:12 +0000

    Update boxkit.sh

--- scripts/boxkit.sh
+++ scripts/boxkit.sh
@@ -7,4 +7,4 @@
 pacman -Syyuu --noconfirm archlinux-keyring cachyos-keyring
 grep -v '^#' ./boxkit.packages | xargs pacman -Sy --noconfirm 
 
-echo "cd /home/$(awk -F: '$3==1000 {print $1}' /run/host/etc/passwd)/.var/app/io.github.MakovWait.Godots/data/godot/app_userdata/Godots/versions/" >> /etc/profile
+echo "cd /home/$( distrobox-host-exec cd ~/ )/.var/app/io.github.MakovWait.Godots/data/godot/app_userdata/Godots/versions/" >> /etc/profile
