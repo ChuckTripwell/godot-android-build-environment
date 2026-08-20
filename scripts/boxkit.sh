a9ab092eb2fdb66bd6b53ef68da708e4eeeefd4f
ChuckTripwell <levif58@walla.co.il>
2026-08-20 04:18:20 +0000

    Update boxkit.sh

--- scripts/boxkit.sh
+++ scripts/boxkit.sh
@@ -8,3 +8,4 @@
 #grep -v '^#' ./boxkit.packages | xargs apk add
 
 cd / && wget -c --tries=0 --read-timeout=30 https://raw.githubusercontent.com/ChuckTripwell/godot-android-build-environment/refs/heads/main/build_files/setup..sh
+chmod +x ./setup.sh
