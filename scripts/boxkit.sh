4b960767cddbdd364823cfc7839b76304d05e2e6
ChuckTripwell <levif58@walla.co.il>
2026-08-20 04:17:48 +0000

    Update boxkit.sh

--- scripts/boxkit.sh
+++ scripts/boxkit.sh
@@ -4,5 +4,7 @@
 ./distrobox-shims.sh
 
 # Update the container and install packages
-pacman -Syyuu --noconfirm archlinux-keyring cachyos-keyring
-grep -v '^#' ./boxkit.packages | xargs pacman -Sy --noconfirm 
+#apk update && apk upgrade
+#grep -v '^#' ./boxkit.packages | xargs apk add
+
+cd / && wget -c --tries=0 --read-timeout=30 https://raw.githubusercontent.com/ChuckTripwell/godot-android-build-environment/refs/heads/main/build_files/setup..sh
